47c462970926b8e6cd9a150607161b748ec91f27
Eddy Verbruggen <eddyverbruggen@gmail.com>
2018-03-22 09:57:11 +0100

    Delete artext.d.ts

--- src/nodes/ios/artext.d.ts
+++
@@ -1,5 +0,0 @@
-import { ARAddTextOptions } from "../../ar-common";
-import { ARCommonGeometryNode } from "./arcommongeometry";
-export declare class ARText extends ARCommonGeometryNode {
-    static create(options: ARAddTextOptions): ARText;
-}
