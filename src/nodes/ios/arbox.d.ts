62d6f97a2751e4216b9c3abaf518f8a8683123a2
Eddy Verbruggen <eddyverbruggen@gmail.com>
2018-03-22 09:56:13 +0100

    Delete arbox.d.ts

--- src/nodes/ios/arbox.d.ts
+++
@@ -1,5 +0,0 @@
-import { ARAddBoxOptions } from "../../ar-common";
-import { ARCommonGeometryNode } from "./arcommongeometry";
-export declare class ARBox extends ARCommonGeometryNode {
-    static create(options: ARAddBoxOptions): ARBox;
-}
