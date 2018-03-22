5c1db6e1c7476573e9451e5cbbf66b82c5e714b1
Eddy Verbruggen <eddyverbruggen@gmail.com>
2018-03-22 09:57:35 +0100

    Delete armodel.d.ts

--- src/nodes/ios/armodel.d.ts
+++
@@ -1,5 +0,0 @@
-import { ARAddModelOptions } from "../../ar-common";
-import { ARCommonNode } from "./arcommon";
-export declare class ARModel extends ARCommonNode {
-    static create(options: ARAddModelOptions): ARModel;
-}
