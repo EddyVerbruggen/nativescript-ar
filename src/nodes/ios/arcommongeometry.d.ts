95943ab4d160dbe677ef20a7ef8d97b8b16d4298
Eddy Verbruggen <eddyverbruggen@gmail.com>
2018-03-22 09:56:40 +0100

    Delete arcommongeometry.d.ts

--- src/nodes/ios/arcommongeometry.d.ts
+++
@@ -1,5 +0,0 @@
-import { ARAddGeometryOptions } from "../../ar-common";
-import { ARCommonNode } from "./arcommon";
-export declare abstract class ARCommonGeometryNode extends ARCommonNode {
-    constructor(options: ARAddGeometryOptions, node: SCNNode);
-}
