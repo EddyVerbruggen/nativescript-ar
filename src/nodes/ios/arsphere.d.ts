ec1fd6a8de89e2d2bd25a0fb4199b26f7a34f1f8
Eddy Verbruggen <eddyverbruggen@gmail.com>
2018-03-22 09:57:27 +0100

    Delete arsphere.d.ts

--- src/nodes/ios/arsphere.d.ts
+++
@@ -1,5 +0,0 @@
-import { ARAddSphereOptions } from "../../ar-common";
-import { ARCommonGeometryNode } from "./arcommongeometry";
-export declare class ARSphere extends ARCommonGeometryNode {
-    static create(options: ARAddSphereOptions): ARSphere;
-}
