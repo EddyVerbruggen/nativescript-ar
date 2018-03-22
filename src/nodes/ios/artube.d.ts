a2371221436f9ad557161f3dce17273102e9d1a9
Eddy Verbruggen <eddyverbruggen@gmail.com>
2018-03-22 09:57:17 +0100

    Delete artube.d.ts

--- src/nodes/ios/artube.d.ts
+++
@@ -1,5 +0,0 @@
-import { ARAddTubeOptions } from "../../ar-common";
-import { ARCommonGeometryNode } from "./arcommongeometry";
-export declare class ARTube extends ARCommonGeometryNode {
-    static create(options: ARAddTubeOptions): ARTube;
-}
