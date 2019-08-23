574274e12cb154a551eb41ae8a730fb6b82a1690
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-08-23 20:51:06 +0200

    implemented addImage for android and ios #51 (linting)

--- src/nodes/ios/arimage.ts
+++ src/nodes/ios/arimage.ts
@@ -1,65 +1,42 @@
+import { fromFileOrResource, fromUrl, ImageSource } from "tns-core-modules/image-source";
 import { ARAddImageOptions } from "../../ar-common";
 import { ARCommonNode } from "./arcommon";
-import { ImageSource, fromFileOrResource, fromUrl } from "tns-core-modules/image-source";
 
-let pixelsPerMeter=500;
+const pixelsPerMeter = 500;
 
 export class ARImage extends ARCommonNode {
- 
-  static create(options: ARAddImageOptions) : Promise<ARImage> {
 
+  static create(options: ARAddImageOptions): Promise<ARImage> {
+    if (typeof options.image === "string") {
 
+      if (options.image.indexOf("://") >= 0) {
+        return fromUrl(options.image).then(function (image) {
+          options.image = image;
+          return ARImage.create(options);
+        });
+      }
+      options.image = fromFileOrResource(options.image);
+    }
 
-  	 if(typeof options.image=="string"){
+    return new Promise<ARImage>(async (resolve, reject) => {
+      const image = (<ImageSource>options.image).ios;
 
-         
-        if(options.image.indexOf("://")>=0){
-	        return fromUrl(options.image).then(function(image){
+      if (!options.dimensions) {
+        options.dimensions = {
+          x: image.size.width / pixelsPerMeter,
+          y: image.size.height / pixelsPerMeter
+        };
+      }
 
+      const dimensions = options.dimensions;
 
-	        	options.image=image;
-	        	return ARImage.create(options);
+      const materialPlane = SCNPlane.planeWithWidthHeight(dimensions.x, dimensions.y);
 
-	        });
-	
-			    }
+      materialPlane.firstMaterial.diffuse.contents = image;
+      materialPlane.firstMaterial.doubleSided = true;
 
-			        options.image=fromFileOrResource(options.image);
-			 
+      resolve(new ARImage(options, SCNNode.nodeWithGeometry(materialPlane)));
 
-		 }
-
-
-
-
-
-
-
-
-  	return new Promise<ARImage>(async (resolve, reject) => {  
-
-
-
-
-	      const image=(<ImageSource>options.image).ios;
-
-
-  		  if(!options.dimensions){
-  		  	options.dimensions={
-  		  		x:image.size.width/pixelsPerMeter,
-  		  		y:image.size.height/pixelsPerMeter
-  		  	}
-  		  }
-
-	      const dimensions=options.dimensions;
-
-	      const materialPlane = SCNPlane.planeWithWidthHeight(dimensions.x, dimensions.y);
-
-	      materialPlane.firstMaterial.diffuse.contents = image;	
-	      materialPlane.firstMaterial.doubleSided=true;
-
-	      resolve(new ARImage(options, SCNNode.nodeWithGeometry(materialPlane)));
-
-  		});
+    });
   }
 }
